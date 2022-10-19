6365b97463245dabed4f42fc67edf10c257d87d2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-10-19 17:48:45 +0700

    Using lambda

--- daniel_koch_lectures/lesson_5_1_v1.py
+++ daniel_koch_lectures/lesson_5_1_v1.py
@@ -1,24 +1,34 @@
 import numpy as np
 import math
 
+# function as first class citizen
+# function is treated similar to variable
+
+# functional programming
+# Haskell, OCaml, Lisp, 
+
+# Sebuah fungsi yang mengembalikan fungsi
 def blackbox_f():
 
-    def F1(x):
-        return 0
+    # Constant functions
+    F1 = lambda x : 0
+    F2 = lambda x : 1
 
-    def F2(x):
-        return 1
-
-    def F3(x):
-        return x%2
-
-    def F4(x):
-        return (x+1)%2
+    # Balanced functions
+    F3 = lambda x : x % 2
+    F4 = lambda x : (x + 1)%2
+    # Bisa juga menggunakan else if
 
     functions = [F1, F2, F3, F4]
+    # generate index integer random: 0, 1, 2, 3
     idx_rand = np.random.randint(0, 4) # 4: exclusive
+
+    print("idx_rand = ", idx_rand)
+
     f = functions[idx_rand]
     return f
+
+
 
 F = blackbox_f()
 print("f(0): ", F(0))
@@ -28,3 +38,4 @@ if F(0) == F(1):
     print("Conclusion: f is constant")
 else:
     print("Conclusion: f is balanced")
+

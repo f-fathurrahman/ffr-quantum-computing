a5b917d551bf81032a9c684f0569b3733c885154
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-07-01 18:47:11 +0700

    Test Julia

---
+++ vqe/test_julia01.jl
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: jl:percent
+#     text_representation:
+#       extension: .jl
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Julia 1.10.4
+#     language: julia
+#     name: julia-1.10
+# ---
+
+# %%
+using LinearAlgebra
+
+# %%
+[1, 2, 3, 4]
+
+# %%
+[1 2 3 4]
+
+# %%
+[1 2; 3 4]
+
+# %%
+[1 2; 3 4 ; 5 6; 7 8 ]
+
+# %%
+[1 2; 3 4]
+
+# %%
+matrix1 = [1.0 1.0 ; 3.1 2.1]
+
+# %%
+[1 2 3]
+
+# %%
+[1 ;; 2 ;; 3]
+
+# %%
+σₓ = [ [1, 0] [0, 1] ]
+
+# %%
+σₓ * σₓ
+
+# %%

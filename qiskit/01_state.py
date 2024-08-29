6b4a4f052f8856216b093331ef23adf323c973d8
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-29 22:49:14 +0700

    Fix for qiskit-1

--- qiskit/01_state.py
+++ qiskit/01_state.py
@@ -1,4 +1,5 @@
-from qiskit import QuantumRegister, QuantumCircuit, Aer, execute
+from qiskit import QuantumRegister, QuantumCircuit, transpile
+from qiskit_aer import Aer
 
 q = QuantumRegister(1)
 hello_qubit = QuantumCircuit(q)
@@ -7,6 +8,6 @@ hello_qubit.id(q[0])
 
 
 S_simulator = Aer.backends(name="statevector_simulator")[0]
-
-job = execute(hello_qubit, S_simulator)
+new_circ = transpile(hello_qubit, S_simulator)
+S_simulator.run(new_circ)
 

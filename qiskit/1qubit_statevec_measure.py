4a905173d95397aefc746c8869ae9af3265841dd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-30 13:34:01 +0700

    1qubit statevec sim with and without measurement

---
+++ qiskit/1qubit_statevec_measure.py
@@ -0,0 +1,19 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import Aer
+
+qc = QuantumCircuit(1)
+
+#qc.id(0)
+#qc.x(0) # Pauli X
+#qc.y(0) # Pauli Y
+#qc.z(0) # Pauli Z
+qc.h(0) # Hadamard
+qc.measure_all()
+
+backend = Aer.get_backend("statevector_simulator")
+tqc = transpile(qc, backend)
+job = backend.run(tqc)
+result = job.result()
+psi = result.get_statevector(tqc)
+print("psi = ", psi)
+

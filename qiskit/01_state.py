988f8b73482590e6f78fc84197a0282343c3701b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-30 10:05:42 +0700

    Simplify, print state vector

--- qiskit/01_state.py
+++ qiskit/01_state.py
@@ -2,12 +2,17 @@ from qiskit import QuantumRegister, QuantumCircuit, transpile
 from qiskit_aer import Aer
 
 q = QuantumRegister(1)
-hello_qubit = QuantumCircuit(q)
+qc = QuantumCircuit(q)
+#qc.id(q[0])
+#qc.x(q[0]) # Pauli X
+#qc.y(q[0]) # Pauli Y
+qc.z(q[0]) # Pauli Z
+#qc.h(q[0]) # Hadamard
 
-hello_qubit.id(q[0])
-
-
-S_simulator = Aer.backends(name="statevector_simulator")[0]
-new_circ = transpile(hello_qubit, S_simulator)
-S_simulator.run(new_circ)
+backend = Aer.backends(name="statevector_simulator")[0]
+tqc = transpile(qc, backend)
+job = backend.run(tqc)
+result = job.result()
+psi = result.get_statevector()
+print("psi = ", psi)
 

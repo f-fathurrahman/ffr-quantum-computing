a1b440d37b48ef812bd57faa4f682bbd09d36d15
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-09-11 12:01:22 +0700

    Remove

--- tutorial_sept2024/materi_4Sept2024/gate_matrix.py
+++
@@ -1,8 +0,0 @@
-import qiskit.circuit.library as qclib
-
-I = qclib.IGate().to_matrix()
-H = qclib.HGate().to_matrix()
-X = qclib.XGate().to_matrix()
-Y = qclib.YGate().to_matrix()
-Z = qclib.ZGate().to_matrix()
-
